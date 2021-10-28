2cb2ef0fb7b7269d85b7b6ed6f842332c3bb8e77
iTzBina <csjunior100@hotmail.com>
2021-10-28 02:51:16 -0300

    first commit

---
+++ src/br/com/dio/primeiroprogramaintelij.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprogramaintelij {
+    public static void main(String[] args) {
+        /*int a = 3;
+          int b = 9;
+            System.out.println("hello world "+ (a+b));
+*/
+        gato gato = new gato();
+
+        System.out.println(gato);
+    }
+
+}
